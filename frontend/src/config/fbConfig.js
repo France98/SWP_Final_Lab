6d97de89529cd7664af2ba2dd35eb97e99b48040
france98 <phanuwatch.l@ku.th>
2019-05-23 20:45:19 +0700

    final lab examination

---
+++ frontend/src/config/fbConfig.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/database";
+
+const apis = {
+  apiKey: "AIzaSyCE6-PAF2G9pCoapYm5ANPovtbiIjl2Mgo",
+  authDomain: "swp-final-exam-49609.firebaseapp.com",
+  apiKey: "AIzaSyD1d1iuLADGxS10TUReUY27rGQJpX6NLXE",
+    authDomain: "finalswp-241511.firebaseapp.com",
+    databaseURL: "https://finalswp-241511.firebaseio.com",
+    projectId: "finalswp-241511",
+    storageBucket: "finalswp-241511.appspot.com",
+    messagingSenderId: "1023835823721",
+    appId: "1:1023835823721:web:a66554b10c1dd88f"
+};
+
+firebase.initializeApp(apis);
+
+export default firebase;
